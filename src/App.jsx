0edaf06a110812a65e56f26823106fbd6f329736
NwaguChima <nwaguchima48@gmail.com>
2022-11-23 01:05:05 +0100

    initial setup and commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.module.scss";
+
+function App() {
+  return <div className="App">Hello App</div>;
+}
+
+export default App;
