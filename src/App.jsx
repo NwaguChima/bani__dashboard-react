6a10c7f25c102f7445c3e0fdd34fb2228c4f1c87
NwaguChima <nwaguchima48@gmail.com>
2022-11-23 02:10:15 +0100

    protected routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,25 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import { Navigate, Route, Routes } from "react-router-dom";
 import Login from "./pages/login";
+import Dashboard from "./pages/dashboard";
 import "./App.module.scss";
-import Layout from "./Outlet";
+import RequireAuth from "./components/AuthCheck";
 
 function App() {
   const [user, setUser] = useState(null);
 
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<Navigate to="/login" />}></Route>
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </div>
+    <Routes>
+      <Route
+        path="/"
+        element={<Navigate to={user ? "dashboard" : "login"} />}
+      />
+
+      <Route path="/login" element={<Login />} />
+      <Route path="/dashboard" element={<RequireAuth />}>
+        <Route index element={<Dashboard />} />
+      </Route>
+    </Routes>
   );
 }
 
