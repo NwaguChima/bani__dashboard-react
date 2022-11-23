5a5a21ec6d35ffaae262f3427d25709d2c862e2f
NwaguChima <nwaguchima48@gmail.com>
2022-11-23 01:52:26 +0100

    setup routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
-import { BrowserRouter, Route, Router } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
+import Login from "./pages/login";
 import "./App.module.scss";
+import Layout from "./Outlet";
 
 function App() {
-  return <div className="App">Hello App</div>;
+  const [user, setUser] = useState(null);
+
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />}></Route>
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
