21400615a96c97f1e282c0206dc23d6c260c0a2d
NwaguChima <nwaguchima48@gmail.com>
2022-11-23 01:22:27 +0100

    setup login component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
+import { BrowserRouter, Route, Router } from "react-router-dom";
 import "./App.module.scss";
 
 function App() {
